b6eaa830ac92b4cb7c5512afca642a61ac02a843
Dzmitry Starastsenka <zagadkatata@list.ru>
2021-09-15 19:52:20 +0300

    Create empty projects
    - Add configuration to project
    - Add DBContext empty class

---
+++ InstagramClone.Infrastructure/DAL/Context/InstagramCloneDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InstagramClone.Infrastructure.DAL.Context
+{
+    public class InstagramCloneDbContext : DbContext
+    {
+
+        public InstagramCloneDbContext(DbContextOptions<InstagramCloneDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.LogTo(System.Console.WriteLine);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
