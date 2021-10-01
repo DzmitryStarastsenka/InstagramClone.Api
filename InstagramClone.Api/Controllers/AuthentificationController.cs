9786491a3d858b9f1b9cdf78dde9780f73b8626a
Dzmitry Starastsenka <zagadkatata@list.ru>
2021-10-01 15:22:06 +0300

    Add authentification and user controllers

---
+++ InstagramClone.Api/Controllers/AuthentificationController.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using InstagramClone.Application.Models.Authentificate;
+using InstagramClone.Application.Models.User;
+using InstagramClone.Application.Services.User.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace InstagramClone.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthentificationController : Controller
+    {
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+
+        public AuthentificationController(
+            IUserService userService,
+            IMapper mapper)
+        {
+            _userService = userService;
+            _mapper = mapper;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<ActionResult<LoginResponse>> Login([FromBody] LoginRequest request)
+        {
+            var user = await _userService.AuthenticateAsync(request);
+
+            if (user == null)
+                return BadRequest();
+
+            var token = _userService.GenerateToken(user);
+
+            return new LoginResponse
+            {
+                UserProfile = user,
+                Token = token
+            };
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        {
+            var userDTO = _mapper.Map<UserProfileDTO>(request);
+            await _userService.CreateAsync(userDTO, request.Password);
+            return Ok();
+        }
+
+        [AllowAnonymous]
+        [HttpPost("validateToken")]
+        public IActionResult ValidateToken([FromBody] ValidateTokenRequest request)
+        {
+            var validateToken = _userService.ValidateToken(request.Token);
+            if (validateToken)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ InstagramClone.Api/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using InstagramClone.Application.Models.User;
+using InstagramClone.Application.Services.User.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Sieve.Models;
+using System.Threading.Tasks;
+
+namespace InstagramClone.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+
+        public UsersController(
+            IUserService userService,
+            IMapper mapper)
+        {
+            _userService = userService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [Route("userProfiles")]
+        public async Task<IActionResult> GetUserProfiles([FromQuery] SieveModel request)
+        {
+            var users = await _userService.GetUserProfiles(request);
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            return Ok(_userService.GetById(id));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateUserRequest request)
+        {
+            var user = _mapper.Map<UserProfileDTO>(request);
+            user.Id = id;
+
+            await _userService.UpdateAsync(user, request.Password);
+            return Ok();
+        }
+    }
+}
