204f00de6d984ff3634824141ab2e72b7d8c5145
Dzmitry Starastsenka <zagadkatata@list.ru>
2021-10-06 22:52:32 +0300

    Create initial user

---
+++ InstagramClone.Infrastructure/Migrations/20211006195143_InitialSeedData.cs
@@ -0,0 +1,29 @@
+﻿using InstagramClone.Application.Helpers;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InstagramClone.Infrastructure.Migrations
+{
+    public partial class InitialSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string adminPassword = "Jaw-15F-aqA-Kvg";
+            PasswordHashHelper.CreatePasswordHash(adminPassword, out byte[] passwordHash, out byte[] passwordSalt);
+
+            migrationBuilder.InsertData(
+                table: "UserProfiles",
+                columns: new[] { "FirstName", "LastName", "UserName", "PasswordHash", "PasswordSalt" },
+                values: new object[,]
+                {
+                    {
+                        "Admin", "Test", "admin@gmail.com", passwordHash, passwordSalt
+                    }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
