76bbd7206912919458b4d643a12f2e15a8355793
Dzmitry Starastenka <zagadkatata@list.ru>
2022-03-31 20:47:50 +0300

    Migrate to .NET 6
    - Update all the dependencies
    - Update Program.cs according to the new structure

--- InstagramClone.Api/Program.cs
+++ InstagramClone.Api/Program.cs
@@ -1,26 +1,154 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using InstagramClone.Api.Extensions;
+using InstagramClone.Api.Filters;
+using InstagramClone.Api.PipelineBehaviours;
+using InstagramClone.Api.Policies.Requirements;
+using InstagramClone.Application.Helpers;
+using InstagramClone.Application.Services.RabbitMQ;
+using InstagramClone.Application.Services.User.Providers;
+using InstagramClone.Application.Sieve;
+using InstagramClone.Application.Validations.Users;
+using InstagramClone.Domain.DAL;
+using InstagramClone.Domain.DAL.Models.Post;
+using InstagramClone.Domain.DAL.Models.User;
+using InstagramClone.Domain.Jwt;
+using InstagramClone.Domain.Jwt.Impl;
+using InstagramClone.Domain.Jwt.Interfaces;
+using InstagramClone.Domain.UserProviders;
+using InstagramClone.Infrastructure.DAL;
+using InstagramClone.Infrastructure.DAL.Context;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Sieve.Services;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
 
-namespace InstagramClone.Api
-{
-    public class Program
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+// Add services to the container.
+
+builder.Services.AddDbContext<InstagramCloneDbContext>(options =>
+    options.UseSqlServer(
+        configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddMvcCore()
+    .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<ValidateAuthJwtTokenCommandValidator>());
+ValidatorOptions.Global.LanguageManager.Culture = new CultureInfo("en-US");
+
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options =>
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        options.SerializerSettings.DateFormatString = "yyyy-MM-ddTHH:mm:ssZ";
+        options.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
+        options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+    });
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+builder.Services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationPipelineBehaviour<,>));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options => JwtHandler.ConfigureJwtBearerOptions(options, configuration.GetSection("JwtToken")));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "InstagramClone", Version = "v1" });
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
+
+builder.Services.AddScoped<IUserJwtTokenGenerator, AuthJwtTokenGenerator>();
+
+builder.Services.AddScoped<ISieveCustomFilterMethods, SieveCustomFilters>();
+builder.Services.AddScoped<ISieveProcessor, ApplicationSieveProcessor>();
+
+builder.Services.AddScoped<IRepository<UserProfile>, EntityRepository<UserProfile>>();
+
+builder.Services.AddScoped<IRepository<UserPost>, EntityRepository<UserPost>>();
+builder.Services.AddScoped<IRepository<PostComment>, EntityRepository<PostComment>>();
+builder.Services.AddScoped<IRepository<PostLike>, EntityRepository<PostLike>>();
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddTransient<IAuthenticatedCurrentUserInfoProvider, AuthenticatedCurrentUserInfoProvider>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "InstagramClone v1"));
 }
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.ConfigureCustomExceptionMiddleware();
+
+app.MapControllers();
+
+app.Run();
